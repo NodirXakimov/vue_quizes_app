1f5a47749e3e8e1ab6ab9c0407cc71f04516da4c
NodirXakimov <nodirxakimov@yandex.ru>
2022-11-29 17:28:47 +0500

    Components retriveing the data from json file now

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,9 +1,14 @@
 <script setup>
-    import Question from '../components/Question.vue' 
-    import QuizHeader from '../components/QuizHeader.vue' 
-    import {useRoute} from 'vue-router' 
+import Question from '../components/Question.vue'
+import QuizHeader from '../components/QuizHeader.vue'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import quizes from '../data/quizes.json'
 
-    const route = useRoute()
+const route = useRoute()
+const quizId = parseInt(route.params.id)
+const quiz = quizes.find(q => q.id === quizId)
+const currentQuestionIndex = ref(0)
 
 </script>
 
